617858d040bc8806aa41f9549bc74a30b59dfad7
Kalin Krustev <kalin.krustev@gmail.com>
2025-07-30 09:29:50 +0000

    fix: support read only root filesystem

--- docker/loadRuntimeConfig.sh
+++ docker/loadRuntimeConfig.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+if grep -q "__REACT_APP_API_BASE_URL__" /usr/share/nginx/html/runtime-env.js; then
 sed -i 's#__REACT_APP_API_BASE_URL__#'"$REACT_APP_API_BASE_URL"'#g' /usr/share/nginx/html/runtime-env.js
 sed -i 's#__REACT_APP_MOCK_API__#'"$REACT_APP_MOCK_API"'#g' /usr/share/nginx/html/runtime-env.js
+else
+  echo "skipping replacement."
+fi
 
 exec "$@"
